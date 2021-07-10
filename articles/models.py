047e6724c935cc1585d48266424719ae222edc40
Rolando Vázquez <rolando.vazquez.23@gmail.com>
2021-07-10 08:03:33 -0600

    Runing validators migrations.

--- articles/models.py
+++ articles/models.py
@@ -4,13 +4,15 @@ from vendors.models import Vendor
 
 from django.urls import reverse
 
+from django.core.validators import *
+
 # Create your models here.
 class Article(models.Model):
 	id = models.AutoField(primary_key = True)
 	code = models.CharField(max_length = 10)
 	description = models.TextField(null = True)
 	vendors = models.ManyToManyField(Vendor)
-	price = models.DecimalField(decimal_places = 4, max_digits = 12)
+	price = models.DecimalField(decimal_places = 4, max_digits = 12, validators=[MinValueValidator(0)])
 
 	def __str__(self):
 		return "%s  -  %s" % (self.code, self.description)
--- catalogs/models.py
+++ catalogs/models.py
@@ -5,16 +5,20 @@ class CustomerType(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length = 60)
 	description = models.TextField()
-	key = models.CharField(max_length = 50)
+	key = models.CharField(max_length = 50, unique = True)
 
 class OrderType(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length = 60)
 	description = models.TextField(null = True)
-	key = models.CharField(max_length = 50)
+	key = models.CharField(max_length = 50, unique = True)
 
 	def __str__(self):
 		return self.name
+
+	def abc_validator(self, xp):
+		print(xp)
+		return True
 
 class Warehouse(models.Model):
 	id = models.AutoField(primary_key = True)
