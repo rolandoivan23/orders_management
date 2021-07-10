ddcbe746f4231198a482da9126c94f1810e38572
Rolando Vázquez <rolando.vazquez.23@gmail.com>
2021-07-10 14:12:55 -0600

    Custom validation for catalogs key pattern matching, it was implemented overwriting clean method in catalogs OrderType and CustomerType

--- catalogs/models.py
+++ catalogs/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.core.exceptions import ValidationError
+
 # Create your models here.
 class CustomerType(models.Model):
 	id = models.AutoField(primary_key=True)
@@ -7,7 +9,11 @@ class CustomerType(models.Model):
 	description = models.TextField()
 	key = models.CharField(max_length = 50, unique = True)
 
+	def clean(self):
+		check_key_pattern(self.key, self.name)
+
 class OrderType(models.Model):
+
 	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length = 60)
 	description = models.TextField(null = True)
@@ -15,12 +21,24 @@ class OrderType(models.Model):
 
 	def __str__(self):
 		return self.name
+	
+	def clean(self):
+		check_key_pattern(self.key, self.name)
 
-	def abc_validator(self, xp):
-		print(xp)
-		return True
 
 class Warehouse(models.Model):
 	id = models.AutoField(primary_key = True)
 	code = models.CharField(max_length = 60)
 	address = models.CharField(max_length = 120)
+
+
+"""
+Recibe 2 cadenas como parámetro y verifica que estas sean iguales una vez que se 
+transforma la cadena de nombre de un registro de un catálogo, ya transformado el 
+nombre debe de ser igual que la llave(campo key) del registro introducido.
+"""
+def check_key_pattern(key, name):
+	cleaned_name = name.replace(' ', '_').lower().strip()
+	if not key == cleaned_name:
+		 raise ValidationError({"key": 'Key pattern not match respect name'})
+	return True
